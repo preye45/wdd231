7239c940f969130635e6dd08a91e6f798b417a54
preye45 <preyetawariowei@gmail.com>
2026-04-04 21:59:11 +0100

    Create discover.mjs

---
+++ chamber/data/discover.mjs
@@ -0,0 +1,67 @@
+
+export const pointsOfInterest = [
+    {
+      id: "Zuma Rock",
+      name: "Zuma Rock",
+      address: "Madalla, Abuja-Kaduna Expressway, Abuja.",
+      image: "images/zuma.webp",
+      alt: "Zuma Rock",
+      description: "Known as the Gateway to Abuja, this massive 725-metre monolith features a natural runoff pattern that resembles a human face.."
+    },
+    {
+      id: "Abuja National Mosque",
+      name: "Abuja National Mosque",
+      address: " 1012 Sani Abacha Way, Central Business District, Abuja.",
+      image: "images/abuja.webp",
+      alt: "Abuja National Mosque",
+      description: "A stunning architectural landmark featuring a large golden dome and four towering minarets, open to the public except during prayers.."
+    },
+    {
+      id: "Millennium Park",
+      name: "Millennium Park",
+      address: "5 Usuma St, Three Arms Zone, Abuja.",
+      image: "images/park.webp",
+      alt: "Millennium Park",
+      description: "The largest public park in Abuja, offering lush landscaped gardens, walking trails, and serene picnic spots in the heart of the city.."
+    },
+    {
+      id: "Jabi Lake Mall",
+      name: "Jabi Lake Mall",
+      address: "Bala Baruwa Way, Jabi, Abuja.",
+      image: "images/jabi.webp",
+      alt: "Jabi Lake Mall",
+      description: "A man-made lake where visitors can enjoy boat rides and high-end shopping at the adjacent Jabi Lake Mall.."
+    },
+    {
+      id: "Thought Pyramid Art Centre, Abuja",
+      name: "Thought Pyramid Art Centre, Abuja",
+      address: "18 Libreville St, Wuse 2, Abuja",
+      image: "images/thought.webp",
+      alt: "Thought Pyramid Art Centre, Abuja",
+      description: " A premier contemporary art gallery showcasing modern African art and hosting regular cultural exhibitions."
+    },
+    {
+      id: "Arts And Crafts Village",
+      name: "Arts And Crafts Village",
+      address: "Sani Abacha Way, Central Business District (beside Sheraton), Abuja",
+      image: "images/arts.webp",
+      alt: "Arts And Crafts Village",
+      description: "A vibrant marketplace with rustic thatched huts where local artisans sell traditional textiles, jewelry, and wood carvings."
+    },
+    {
+      id: "National Childrens Park & Zoo Abuja",
+      name: "National Childrens Park & Zoo Abuja",
+      address: "Asokoro District (behind Aso Rock), Abuja.",
+      image: "images/national.webp",
+      alt: "National Childrens Park & Zoo Abuja",
+      description: " A family-friendly destination featuring diverse animal exhibits, playgrounds, and education programs for children."
+    },
+    {
+      id: "Aso Rock",
+      name: "Aso Rock",
+      address: "Three Arms Zone, Abuja",
+      image: "images/aso.webp",
+      alt: "Aso Rock",
+      description: " A 400-metre granite outcrop that serves as the backdrop to the Presidential Villa and National Assembly, representing the political heart of Nigeria.."
+    }
+  ];
